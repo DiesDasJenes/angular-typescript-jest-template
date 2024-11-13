1b9438b9146cc41607a4f0b5b88ff3b5c961129f
Philipp Ludewig <diesdas@philippludewig.de>
2024-11-13 16:35:00 -0500

    Fixed compiler error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import { ReactiveFormsModule } from '@angular/forms';
 
-const config: SocketIoConfig = { url: 'http://localhost:1337', options: {} };
 
 @NgModule({
   declarations: [
@@ -17,7 +15,6 @@ const config: SocketIoConfig = { url: 'http://localhost:1337', options: {} };
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SocketIoModule.forRoot(config),
     ReactiveFormsModule
   ],
   providers: [],
